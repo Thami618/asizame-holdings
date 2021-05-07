4180655d192a6718e24bd945016edeb31558fd44
Thami618 <thamngcobo1998@gmail.com>
2021-05-07 12:26:37 +0200

    my first netbeans commit

---
+++ Documents/NetBeansProjects/arrays/src/arrays/Arrays.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrays;
+
+/**
+ *
+ * @author Student
+ */
+public class Arrays {
+
+    public static void main(String[] args) {
+        int passmark = 12;
+        boolean passed = false;
+        int[] scores = {4,6,2,8,12,35,9};
+        for (int unitScore : scores){
+            if(unitScore >= 12){
+                passed = true;
+                break;
+            }
+        }
+        System.out.println("At the "+ passed);
+    }
+    
+}
+
